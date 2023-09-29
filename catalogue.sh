b2e18b118b0a9f90aa6485fb6ee98be66927ca7a
chinnapareddy <chinna.aduri@gmail.com>
2023-09-29 00:09:44 -0700

    Catalogue Log file creation

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,43 @@
-cp catalogue.service  /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+log=/tmp/roboshop.log
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-npm install
+echo -e "\e[36m>>>>>>>>>>>> Create Catalogue Service <<<<<<<<< \e[0m"
+cp catalogue.service  /etc/systemd/system/catalogue.service &>>${log}
 
-dnf install mongodb-org-shell -y
-mongo --host mongodb.devops-tools.online </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>>>>> Create Mongodb Repo <<<<<<<<< \e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+echo -e "\e[36m>>>>>>>>>>>> Install Nodejs Repo<<<<<<<<< \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Install NodeJS <<<<<<<<< \e[0m"
+dnf install nodejs -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Create Application User <<<<<<<<< \e[0m"
+useradd roboshop &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Create Application Directory <<<<<<<<< \e[0m"
+rm -rf /app &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Create Application Directory <<<<<<<<< \e[0m"
+mkdir /app &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Download Application Content <<<<<<<<< \e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Extract Application Content  <<<<<<<<< \e[0m"
+cd /app &>>${log}
+unzip /tmp/catalogue.zip &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Download NodeJS Dependencies <<<<<<<<< \e[0m"
+npm install &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Install Mongo Client <<<<<<<<< \e[0m"
+dnf install mongodb-org-shell -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Load Catalogue schema <<<<<<<<< \e[0m"
+mongo --host mongodb.devops-tools.online </app/schema/catalogue.js &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>> Start catalogue Services <<<<<<<<< \e[0m"
+systemctl daemon-reload &>>${log}
+systemctl enable catalogue &>>${log}
+systemctl restart catalogue &>>${log}
--- redis.sh
+++ redis.sh
@@ -5,4 +5,4 @@ dnf install redis -y
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf & /etc/redis/redis.conf
 
 systemctl enable redis
-systemctl restart redis
+systemctl restart redis  
