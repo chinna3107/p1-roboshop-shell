fd459010a3f554849a288e0430061dae1a5ad0e1
chinnapareddy <chinna.aduri@gmail.com>
2023-09-29 22:04:41 -0700

    functions in Catalogue

--- user.sh
+++ user.sh
@@ -1,21 +1,5 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+component=user
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
+source common.sh
 
-dnf install mongodb-org-shell -y
-mongo --host mongodb.devops-tools.online </app/schema/user.js
-
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
-
-
+func_user
